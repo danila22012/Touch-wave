f444e4b041dbc9add0db8225ef048806ba521118
sadychenkoDV <sadychenkoDV@gmail.com>
2021-05-14 16:31:48 +0300

    change modal settings to page settings

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,39 +1,12 @@
 import React, { useState } from "react";
-import SettingsPortal from "../../components/SettingsPortal/SettingsPortal";
 import Cross from "../../static/Cross.svg";
 import styles from "./styles.module.css";
-import { Modal, Button } from "react-bootstrap";
-
 
 const Settings = () => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   return (
-    <SettingsPortal>
-        <h1>settings modal</h1>
-      {/* <React.Fragment>
-        <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
-        </Button>
-
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </React.Fragment> */}
-    </SettingsPortal>
+    <div className={styles.settingsContainer}>
+      <p className={styles.pageTitle}>Settings</p>
+    </div>
   );
 };
 export default Settings;
