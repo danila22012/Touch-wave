535e0fea5d76ff56e4c0e82d75862d14de0eb65e
Yehor Velykozhon <egor1velikogon@gmail.com>
2021-05-15 23:24:18 +0300

    Scripts: add scripts for creating, show, drop all tables

---
+++ drop_all_tables.sql
@@ -0,0 +1,8 @@
+DROP TABLE user_profile;
+DROP TABLE image_storage;
+
+DROP TABLE message_storage;
+DROP TABLE convesation;
+
+DROP TABLE list_of_contacts;
+DROP TABLE user_info;
---
+++ show_all_info_all_tables.sql
@@ -0,0 +1,6 @@
+SELECT * FROM user_info;
+SELECT * FROM list_of_contacts;
+SELECT * FROM image_storage;
+SELECT * FROM user_profile;
+SELECT * FROM convesation;
+SELECT * FROM message_storage;
