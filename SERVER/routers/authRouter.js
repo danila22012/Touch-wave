addbee19e5f9eb50cb4bf43ec9a42ec70bff39f8
Alex Pyatovolenko <piatovolenkoo@gmail.com>
2021-05-20 21:42:34 +0300

    SERVER: fixing userController8

--- SERVER/routers/authRouter.js
+++ SERVER/routers/authRouter.js
@@ -7,6 +7,7 @@ const authMiddleware = require('../middlewaree/authMiddleware')
 
 router.post('/registration', [
     check('username', "Имя пользователя не может быть пустым").notEmpty(),
+    check('secondname', "Фамилия не может быть пустой").notEmpty(),
     check('password', "Пароль должен быть больше 4 и меньше 10 символов").isLength({min:4, max:10})
 ], controller.registration)
 router.post('/login', controller.login)
--- SERVER/routers/userRouter.js
+++ SERVER/routers/userRouter.js
@@ -5,6 +5,7 @@ const authMiddleware = require('../middlewaree/authMiddleware')
 
 router.get('/getAllUsers', authMiddleware, controller.getAllUsers)
 router.get('/getUser', authMiddleware, controller.getUser)
+router.get('/getMyUser', authMiddleware, controller.getMyUser)
 
 // test
 router.post('/createContactList', controller.createContactList)
@@ -12,11 +13,11 @@ router.put('/updateSettings', authMiddleware, controller.updateSettings)
 
 router.get('/getAllContacts', authMiddleware, controller.getAllContacts)
 router.post('/addContact', authMiddleware, controller.addContact)
-router.delete('/deleteContact', authMiddleware, controller.deleteContact)
-router.delete('/getContact', authMiddleware, controller.getContact)
+router.post('/deleteContact', authMiddleware, controller.deleteContact)
+router.post('/getContact', authMiddleware, controller.getContact)
 
 router.get('/getAllDialogs', authMiddleware, controller.getAllDialogs)
-router.delete('/deleteDialog', authMiddleware, controller.deleteDialog)
+router.post('/deleteDialog', authMiddleware, controller.deleteDialog)
 
 // test
 router.post('/createMessage', controller.createMessage)
